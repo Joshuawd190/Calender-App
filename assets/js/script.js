88f4fd8dc38cb179d39be28d2f1c4397a112be47
Joshua D Wood <joshuawd190@gmail.com>
2021-04-25 19:24:15 -0500

    fixed file paths, updated styling

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//style based on time
+var currentDate = moment().format("dddd, MMMM Do");
+console.log(currentDate);
+
+$("currentDay").text(currentDate);
+//update every hour
+//insert date at top of page
+//click events for box and buttons
+//box to text area
+//button to save
+//style button based on hover
+//save content to storage
+//display saved schedule on load
+//clear schedule if day is different than saved
