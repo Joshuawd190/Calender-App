54facc78aac2fe982a220eed323118d6e3b3fa37
Joshua D Wood <joshuawd190@gmail.com>
2021-04-25 20:58:16 -0500

    Time based styling complete, some text input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,58 @@
-//style based on time
+//insert date at top of page
 var currentDate = moment().format("dddd, MMMM Do");
 console.log(currentDate);
-
-$("currentDay").text(currentDate);
+$("#currentDay").text(currentDate);
+//style based on time
 //update every hour
-//insert date at top of page
+var currentHour = moment().format("ha");
+console.log(currentHour);
+
+let timeIndex = [
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
+setInterval(function () {
+  for (i = 0; i < timeIndex.length; i++) {
+    let taskTime = $("#" + timeIndex[i]).attr("id");
+    let taskTimeObj = moment(taskTime, "ha");
+    console.log(taskTimeObj);
+    if (taskTimeObj.isBefore(moment())) {
+      $("#" + timeIndex[i]).addClass("past");
+    } else if (taskTimeObj.isAfter(moment())) {
+      $("#" + timeIndex[i]).addClass("future");
+    } else {
+      $("#" + timeIndex[i]).addClass("present");
+    }
+  }
+}, 3600000);
+
 //click events for box and buttons
+$(".col-10").on("click", function () {
+  var text = $(this).text().trim();
+
+  var textInput = $("<textarea>").addClass("col-10").val(text);
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus");
+});
+$("col-10").on("blur", "textarea", function () {
+  var text = $(this).val();
+  var index = $(this).attr("id");
+
+  var taskP = $("<p>").addClass("col-10").text(text);
+  $(this).replaceWith(taskP);
+});
+
+var saveTask = function () {};
 //box to text area
 //button to save
-//style button based on hover
 //save content to storage
 //display saved schedule on load
 //clear schedule if day is different than saved
